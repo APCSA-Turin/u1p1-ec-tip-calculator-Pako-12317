5fe5f6249d905f7db083599b365e7fe1d173f5b1
Pako-12317 <patrykm10@nycstudents.net>
2024-09-21 17:58:47 +0000

    ,commit

--- src/main/java/com/example/project/ExtraCredit.java
+++ src/main/java/com/example/project/ExtraCredit.java
@@ -1,4 +1,7 @@
 package com.example.project;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -6,11 +9,29 @@ public class ExtraCredit {
 
 
     public static String calculateTip(int people, int percent, double cost, String items) {
-        StringBuilder result = new StringBuilder();
 
-                //COPY AND PASTE YOUR CODE HERE 
+         StringBuilder result = new StringBuilder();
+// I don't like decimal format.
+        String pattern = "##.##";
+        DecimalFormat decimalFormat = new DecimalFormat(pattern);
 
-        //the two lines  should go below result.append("-------------------------------\n"); 
+        double perPersonCostNoTip = (cost/people);
+        double totalTip = (cost*((double)percent/100));
+        double costWithTip = (cost+totalTip);
+        double perPersonWithTip = (costWithTip/people);
+        double tipPerPerson = (totalTip/people);
+        
+        result.append("-------------------------------\n");
+        result.append("Total bill before tip: $"+decimalFormat.format(cost)+"\n"); 
+        result.append("Total percentage: "+decimalFormat.format(percent)+"%"+"\n");
+        result.append("Total tip: $"+decimalFormat.format(totalTip)+"\n");
+        result.append("Total Bill with tip: $"+decimalFormat.format(cost+totalTip)+"\n");
+        result.append("Per person cost before tip: $"+decimalFormat.format(perPersonCostNoTip)+"\n");
+        result.append("Tip per person: $"+decimalFormat.format(tipPerPerson)+"\n");
+        result.append("Total cost per person: $"+decimalFormat.format(perPersonWithTip)+"\n");
+        result.append("-------------------------------\n");
+
+        
         result.append("Items ordered:\n");
         result.append(items);
 
@@ -19,13 +40,20 @@ public class ExtraCredit {
     }
                                    
     public static void main(String[] args) {
-        int people;
-        int percent;
-        double cost;
+        int people = 2;
+        int percent = 5;
+        double cost = 29.56;
         String items; 
-
+        List<String> list = new ArrayList<String>();
+        Scanner scan = new Scanner(System.in);
+        while(list.contains("-1") == false){
+            System.out.println("Enter an item name or type -1 to finish:");
+            list.add(scan.nextLine());
+        }
+        items = list.get(0)+" \n"+list.get(1)+" \n"+list.get(2)+" \n"+list.get(3)+" \n"+list.get(4);
         //Your scanner object and while loop should go here
                              
         System.out.println(calculateTip(people,percent,cost,items));
+        scan.close();
     }
 }
